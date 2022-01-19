13eac2d30607f0fdeaf89b929bea066474fccafb
Allan Dimis <allandimis@hotmail.com.br>
2022-01-19 20:53:10 -0300

    primeiro bot

---
+++ Topics/Declaring a function/Prevent cheating/main.py
@@ -0,0 +1,13 @@
+import math
+
+
+# your code here
+
+def new_math_factorial(num):
+    print("Don't cheat!")
+
+
+math.factorial = new_math_factorial
+
+# don't delete this line, please
+math.factorial(23)
